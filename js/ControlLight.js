28f8b00ec203136cc2ed985b013e6e5371780029
Da-Xian <41809053+Da-Xian@users.noreply.github.com>
2019-03-21 16:34:52 +0800

    Update ControlLight.js

--- js/ControlLight.js
+++ js/ControlLight.js
@@ -96,5 +96,17 @@ canvas.addEventListener(on.end, function(e) {
 }, false);
 
 draw(0);
+var move=function(e){
+e.preventDefault && e.preventDefault();
+e.returnValue=false;
+e.stopPropagation && e.stopPropagation();
+return false;
+    }
+    var keyFunc=function(e){
+if(37<=e.keyCode && e.keyCode<=40){
+return move(e);
+}
+}
+    document.body.onkeydown=keyFunc;
 	
 
